c708b1e3656f2f56b5f4282718ee78ea6b1e66cd
Cyril Bur <cyril.bur@au1.ibm.com>
2017-06-19 15:52:53 +1000

    libflash/mbox-flash: Understand v3

--- libflash/mbox-flash.c
+++ libflash/mbox-flash.c
@@ -72,20 +72,39 @@ struct mbox_flash_data {
 static mbox_handler mbox_flash_do_nop;
 static mbox_handler mbox_flash_do_illegal;
 
-/* Version 1 and Version 2 compatible */
+/* Version 1, 2, 3 compatible */
 static mbox_handler mbox_flash_do_get_mbox_info;
 
+/* Version 2 and 3 compatible */
 static mbox_handler mbox_flash_do_get_flash_info_v2;
 static mbox_handler mbox_flash_do_get_flash_info_v1;
 
+/* Version 2 and 3 compatible */
 static mbox_handler mbox_flash_do_create_read_window_v2;
 static mbox_handler mbox_flash_do_create_read_window_v1;
 
+/* Version 2 and 3 compatible */
 static mbox_handler mbox_flash_do_create_write_window_v2;
 static mbox_handler mbox_flash_do_create_write_window_v1;
 
-/* Version 1 and Version 2 compatible */
+/* Version 1, 2, 3 compatible */
 static mbox_handler mbox_flash_do_close_window;
+
+/* Plus one, commands start at 1 */
+static mbox_handler *handlers_v3[MBOX_COMMAND_COUNT + 1] = {
+	NULL,
+	&mbox_flash_do_nop,
+	&mbox_flash_do_get_mbox_info,
+	&mbox_flash_do_get_flash_info_v2,
+	&mbox_flash_do_create_read_window_v2,
+	&mbox_flash_do_close_window,
+	&mbox_flash_do_create_write_window_v2,
+	&mbox_flash_do_nop,
+	&mbox_flash_do_nop,
+	&mbox_flash_do_nop,
+	&mbox_flash_do_nop,
+	&mbox_flash_do_nop
+};
 
 /* Plus one, commands start at 1 */
 static mbox_handler *handlers_v2[MBOX_COMMAND_COUNT + 1] = {
@@ -99,7 +118,8 @@ static mbox_handler *handlers_v2[MBOX_COMMAND_COUNT + 1] = {
 	&mbox_flash_do_nop,
 	&mbox_flash_do_nop,
 	&mbox_flash_do_nop,
-	&mbox_flash_do_nop
+	&mbox_flash_do_nop,
+	&mbox_flash_do_illegal
 };
 
 /*
@@ -119,6 +139,7 @@ static mbox_handler *handlers_v1[MBOX_COMMAND_COUNT + 1] = {
 	&mbox_flash_do_nop,
 	&mbox_flash_do_nop,
 	&mbox_flash_do_nop,
+	&mbox_flash_do_illegal,
 	&mbox_flash_do_illegal
 };
 
@@ -412,22 +433,26 @@ static void mbox_flash_do_illegal(struct mbox_flash_data *mbox_flash __unused,
 	prlog(PR_CRIT, "Got response to unknown message type\n");
 }
 
-/* Version 1 and Version 2 compatible */
+/* Version 1, 2 and 3 compatible */
 static void mbox_flash_do_get_mbox_info(struct mbox_flash_data *mbox_flash,
 		struct bmc_mbox_msg *msg)
 {
 
 	mbox_flash->version = msg_get_u8(msg, 0);
-	if (mbox_flash->version == 1) {
-		/* Not all version 1 daemons set argument 5 correctly */
-		mbox_flash->shift = 12; /* Protocol hardcodes to 4K anyway */
-		mbox_flash->read.size = blocks_to_bytes(mbox_flash, msg_get_u16(msg, 1));
-		mbox_flash->write.size = blocks_to_bytes(mbox_flash, msg_get_u16(msg, 3));
-	} else { /* V2 compatible */
-		mbox_flash->shift = msg_get_u8(msg, 5);
-		mbox_flash->timeout = msg_get_u16(msg, 6);
-		if (mbox_flash->timeout == 0)
-			mbox_flash->timeout = MBOX_DEFAULT_TIMEOUT;
+	switch (mbox_flash->version) {
+		case 1:
+			/* Not all version 1 daemons set argument 5 correctly */
+			mbox_flash->shift = 12; /* Protocol hardcodes to 4K anyway */
+			mbox_flash->read.size = blocks_to_bytes(mbox_flash, msg_get_u16(msg, 1));
+			mbox_flash->write.size = blocks_to_bytes(mbox_flash, msg_get_u16(msg, 3));
+			break;
+		case 3:
+		case 2:
+			mbox_flash->shift = msg_get_u8(msg, 5);
+			mbox_flash->timeout = msg_get_u16(msg, 6);
+			if (mbox_flash->timeout == 0)
+				mbox_flash->timeout = MBOX_DEFAULT_TIMEOUT;
+			break;
 	}
 	/* Callers will handle the case where the version is not known
 	 *
@@ -438,6 +463,7 @@ static void mbox_flash_do_get_mbox_info(struct mbox_flash_data *mbox_flash,
 	 */
 }
 
+/* Version 2 and 3 compatible */
 static void mbox_flash_do_get_flash_info_v2(struct mbox_flash_data *mbox_flash,
 		struct bmc_mbox_msg *msg)
 {
@@ -452,6 +478,7 @@ static void mbox_flash_do_get_flash_info_v1(struct mbox_flash_data *mbox_flash,
 	mbox_flash->erase_granule = msg_get_u32(msg, 4);
 }
 
+/* Version 2 and 3 compatible */
 static void mbox_flash_do_create_read_window_v2(struct mbox_flash_data *mbox_flash,
 		struct bmc_mbox_msg *msg)
 {
@@ -470,6 +497,7 @@ static void mbox_flash_do_create_read_window_v1(struct mbox_flash_data *mbox_fla
 	mbox_flash->write.open = false;
 }
 
+/* Version 2 and 3 compatible */
 static void mbox_flash_do_create_write_window_v2(struct mbox_flash_data *mbox_flash,
 		struct bmc_mbox_msg *msg)
 {
@@ -997,27 +1025,27 @@ static int protocol_init(struct mbox_flash_data *mbox_flash)
 	struct bmc_mbox_msg *msg;
 	int rc;
 
-	/* Assume V2 */
+	/* Assume V2+ */
 	mbox_flash->bl.read = &mbox_flash_read;
 	mbox_flash->bl.write = &mbox_flash_write;
 	mbox_flash->bl.erase = &mbox_flash_erase_v2;
 	mbox_flash->bl.get_info = &mbox_flash_get_info;
 
-	/* Assume V2 */
-	mbox_flash->handlers = handlers_v2;
+	/* Assume V3 */
+	mbox_flash->handlers = handlers_v3;
 
 	bmc_mbox_register_callback(&mbox_flash_callback, mbox_flash);
 	bmc_mbox_register_attn(&mbox_flash_attn, mbox_flash);
 
 	/*
 	 * For V1 of the protocol this is fixed.
-	 * V2: The init code will update this
+	 * V2+: The init code will update this
 	 */
 	mbox_flash->shift = 12;
 
 	/*
 	 * For V1 we'll use this value.
-	 * V2: The init code (may) update this
+	 * V2+: The init code (may) update this
 	 */
 	mbox_flash->timeout = MBOX_DEFAULT_TIMEOUT;
 
@@ -1027,13 +1055,14 @@ static int protocol_init(struct mbox_flash_data *mbox_flash)
 	 * talk the highest version, we'll update this variable then if
 	 * our highest version is not supported
 	 */
-	mbox_flash->version = 2;
+	mbox_flash->version = 3;
 
 	msg = msg_alloc(mbox_flash, MBOX_C_GET_MBOX_INFO);
 	if (!msg)
 		return FLASH_ERR_MALLOC_FAILED;
 
 	msg_put_u8(msg, 0, mbox_flash->version);
+	msg_put_u8(msg, 1, 0); /* Don't request a shift, let the BMC give us one */
 	rc = msg_send(mbox_flash, msg, mbox_flash->timeout);
 	if (rc) {
 		prlog(PR_ERR, "Failed to enqueue/send BMC MBOX message\n");
@@ -1056,6 +1085,9 @@ static int protocol_init(struct mbox_flash_data *mbox_flash)
 			break;
 		case 2:
 			mbox_flash->handlers = handlers_v2;
+			break;
+		case 3:
+			mbox_flash->handlers = handlers_v3;
 			break;
 		default:
 			/*
@@ -1088,7 +1120,7 @@ int mbox_flash_init(struct blocklevel_device **bl)
 	if (!mbox_flash)
 		return FLASH_ERR_MALLOC_FAILED;
 
-	/* Assume V2 */
+	/* Assume V2+ */
 	mbox_flash->bl.read = &mbox_flash_read;
 	mbox_flash->bl.write = &mbox_flash_write;
 	mbox_flash->bl.erase = &mbox_flash_erase_v2;
