cyrilbur-ibm/skiboot

commit ae6cb86c277e6a9b1d11548d8fa1b430116dba08

external/pflash: Reinstate the progress bars
Recent work did some optimising which unfortunately removed some of the
progress bars in pflash.

It turns out that there's only one thing people prefer to correctly
programmed flash chips, it is the ability to watch little equals
characters go across their screens for potentially minutes.

Personally I don't understand the appeal but I have received strongly
worded requests for the reinstatement of the progress bars in pflash and
I fear for the stability of our society if pflash doesn't promptly regain
its most unimportant feature.

Signed-off-by: Cyril Bur <cyril.bur@au1.ibm.com>
Reviewed-by: Samuel Mendoza-Jonas <sam@mendozajonas.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

external/pflash/pflash.c
@@ -256,17 +256,18 @@ static struct ffs_handle *lookup_partition_at_side(struct flash_details *flash,
 	return lookup_partition_at_toc(flash, name, index);
 }
 
-static int erase_chip(struct blocklevel_device *bl)
+static int erase_chip(struct flash_details *flash)
 {
 	bool confirm;
 	int rc;
+	uint64_t pos;
 
 	printf("About to erase chip !\n");
 	confirm = check_confirm();
 	if (!confirm)
 		return 1;
 
-	printf("Erasing... (may take a while !) ");
+	printf("Erasing... (may take a while)\n");
 	fflush(stdout);
 
 	if (dummy_run) {
@@ -274,20 +275,33 @@ static int erase_chip(struct blocklevel_device *bl)
 		return 1;
 	}
 
-	rc = arch_flash_erase_chip(bl);
+	/*
+	 * We could use arch_flash_erase_chip() here BUT everyone really
+	 * likes the progress bars.
+	 * Lets do an erase block at a time erase then...
+	 */
+	progress_init(flash->total_size);
+	for (pos = 0; pos < flash->total_size; pos += flash->erase_granule) {
+		rc = blocklevel_erase(flash->bl, pos, flash->erase_granule);
+		if (rc)
+			break;
+		progress_tick(pos);
+	}
+	progress_end();
 	if (rc) {
 		fprintf(stderr, "Error %d erasing chip\n", rc);
-		return 1;
+		return rc;
 	}
 
 	printf("done !\n");
 	return 0;
 }
 
-static int erase_range(struct blocklevel_device *bl,
+static int erase_range(struct flash_details *flash,
 		uint32_t start, uint32_t size, bool will_program,
 		struct ffs_handle *ffsh, int ffs_index)
 {
+	uint32_t done = 0, erase_mask = flash->erase_granule - 1;
 	bool confirm;
 	int rc;
 
@@ -302,12 +316,45 @@ static int erase_range(struct blocklevel_device *bl,
 	}
 
 	printf("Erasing...\n");
-	rc = blocklevel_smart_erase(bl, start, size);
-	if (rc) {
-		fprintf(stderr, "Failed to blocklevel_smart_erase(): %d\n", rc);
-		return 1;
+	/*
+	 * blocklevel_smart_erase() can do the entire thing in one call
+	 * BUT everyone really likes progress bars so break stuff up
+	 */
+	progress_init(size);
+	if (start & erase_mask) {
+		/* Align to next erase block */
+		rc = blocklevel_smart_erase(flash->bl, start,
+				flash->erase_granule - (start & erase_mask));
+		if (rc) {
+			fprintf(stderr, "Failed to blocklevel_smart_erase(): %d\n", rc);
+			return 1;
+		}
+		start += flash->erase_granule - (start & erase_mask);
+		size -= flash->erase_granule - (start & erase_mask);
+		done = flash->erase_granule - (start & erase_mask);
 	}
-
+	progress_tick(done);
+	while (size & ~(erase_mask)) {
+		rc = blocklevel_smart_erase(flash->bl, start, flash->erase_granule);
+		if (rc) {
+			fprintf(stderr, "Failed to blocklevel_smart_erase(): %d\n", rc);
+			return 1;
+		}
+		start += flash->erase_granule;
+		size -= flash->erase_granule;
+		done += flash->erase_granule;
+		progress_tick(done);
+	}
+	if (size) {
+		rc = blocklevel_smart_erase(flash->bl, start, size);
+		if (rc) {
+			fprintf(stderr, "Failed to blocklevel_smart_erase(): %d\n", rc);
+			return 1;
+		}
+		done += size;
+		progress_tick(done);
+	}
+	progress_end();
 	/* If this is a flash partition, mark it empty if we aren't
 	 * going to program over it as well
 	 */
@@ -318,7 +365,7 @@ static int erase_range(struct blocklevel_device *bl,
 	return 0;
 }
 
-static int set_ecc(struct blocklevel_device *bl, uint32_t start, uint32_t size)
+static int set_ecc(struct flash_details *flash, uint32_t start, uint32_t size)
 {
 	uint32_t i = start + 8;
 	uint8_t ecc = 0;
@@ -329,12 +376,12 @@ static int set_ecc(struct blocklevel_device *bl, uint32_t start, uint32_t size)
 	if (!confirm)
 		return 1;
 
-	erase_range(bl, start, size, true, NULL, 0);
+	erase_range(flash, start, size, true, NULL, 0);
 
 	printf("Programming ECC bits...\n");
 	progress_init(size);
 	while (i < start + size) {
-		blocklevel_write(bl, i, &ecc, sizeof(ecc));
+		blocklevel_write(flash->bl, i, &ecc, sizeof(ecc));
 		i += 9;
 		progress_tick(i - start);
 	}
@@ -1071,15 +1118,15 @@ int main(int argc, char *argv[])
 	if (do_read)
 		rc = do_read_file(flash.bl, read_file, address, read_size);
 	if (!rc && erase_all)
-		rc = erase_chip(flash.bl);
+		rc = erase_chip(&flash);
 	else if (!rc && erase)
-		rc = erase_range(flash.bl, address, write_size,
+		rc = erase_range(&flash, address, write_size,
 				program, ffsh, ffs_index);
 	if (!rc && program)
 		rc = program_file(flash.bl, write_file, address, write_size,
 				ffsh, ffs_index);
 	if (!rc && do_clear)
-		rc = set_ecc(flash.bl, address, write_size);
+		rc = set_ecc(&flash, address, write_size);
 
 close:
 	if (flash.need_relock)

commit f3b9a8084f0f9dc30ffbd742da9356ae6eb57004

external/pflash: Fix erasing within a single erase block
It is possible to erase within a single erase block. Currently the
pflash code assumes that if the erase starts part way into an erase
block it is because it needs to be aligned up to the boundary with the
next erase block.

Doing an erase smaller than a single erase block will cause underflows
and looping forever on erase.

Fixes: ae6cb86c2 ("external/pflash: Reinstate the progress bars")

---
Side note, this bug is only possible on 64K erase granule platforms.
Also, this bug cannot be caught by our test suite as it stands because
the test suite tests with the file backend to blocklevel and it has an
erase granule of one. I'll have a think about perhaps adding changes so
that we might be able to catch this.

external/pflash/pflash.c
@@ -325,16 +325,20 @@ static int erase_range(struct flash_details *flash,
 	 */
 	progress_init(size);
 	if (start & erase_mask) {
-		/* Align to next erase block */
-		rc = blocklevel_smart_erase(flash->bl, start,
-				flash->erase_granule - (start & erase_mask));
+		/*
+		 * Align to next erase block, or just do the entire
+		 * thing if we fit within one erase block
+		 */
+		uint32_t first_size = MIN(size, (flash->erase_granule - (start & erase_mask)));
+
+		rc = blocklevel_smart_erase(flash->bl, start, first_size);
 		if (rc) {
 			fprintf(stderr, "Failed to blocklevel_smart_erase(): %d\n", rc);
 			return 1;
 		}
-		size -= flash->erase_granule - (start & erase_mask);
-		done = flash->erase_granule - (start & erase_mask);
-		start += flash->erase_granule - (start & erase_mask);
+		size -= first_size;
+		done = first_size;
+		start += first_size;
 	}
 	progress_tick(done);
 	while (size & ~(erase_mask)) {

libflash/libflash.c
@@ -23,10 +23,6 @@
 #include "ecc.h"
 #include "blocklevel.h"
 
-#ifndef MIN
-#define MIN(a, b)	((a) < (b) ? (a) : (b))
-#endif
-
 static const struct flash_info flash_info[] = {
 	{ 0xc22018, 0x01000000, FL_ERASE_ALL | FL_CAN_4B, "Macronix MXxxL12835F"},
 	{ 0xc22019, 0x02000000, FL_ERASE_ALL | FL_CAN_4B, "Macronix MXxxL25635F"},

libflash/libflash.h
@@ -28,6 +28,10 @@
  */
 #include <libflash/errors.h>
 
+#ifndef MIN
+#define MIN(a, b)	((a) < (b) ? (a) : (b))
+#endif
+
 /* Flash chip, opaque */
 struct flash_chip;
 struct spi_flash_ctrl;